b9a286c549631b69248f884282117472a6331443
Chu-Te <ctchen002@gmail.com>
2020-08-03 16:05:39 +0800

    unit test & deployment

--- miniLibrary/minilibrary/minilibrary/settings.py
+++ miniLibrary/minilibrary/minilibrary/settings.py
@@ -20,10 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'o3ywx$-#jj=@7esgc@qfqya=8h5as!ti^+t0f&karrq5!iv#@u'
+#SECRET_KEY = 'o3ywx$-#jj=@7esgc@qfqya=8h5as!ti^+t0f&karrq5!iv#@u'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
